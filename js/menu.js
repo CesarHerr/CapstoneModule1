c1a5db389c6256e9cebb7a03b2297ffb4fc6b876
Cesar Herrera <cesarherr321@gmail.com>
2023-04-18 13:54:27 -0400

    Update Scss files
    Add styles for desktop vertion on main page

--- js/menu.js
+++ js/menu.js
@@ -1,24 +1,21 @@
 const menuIcon = document.querySelector('.hamburger-open');
 const menu = document.querySelector('.header__links--menu');
-// const logo = document.querySelector('.logo-header');
+const logo = document.querySelector('.header__links--logo');
 const closeBtn = document.querySelector('.hamburger-close');
-// const email = document.querySelector('.email-icon');
 const nav = document.querySelector('.header__links');
 
 menuIcon.addEventListener('click', () => {
   menu.classList.toggle('show');
   menuIcon.classList.toggle('show-icon');
-  // logo.classList.toggle('visible');
-  closeBtn.classList.toggle('close');
-  // email.classList.toggle('visible');
+  logo.classList.toggle('visible');
+  closeBtn.classList.toggle('close'); 
   nav.classList.toggle('change-width');
 });
 
 menu.addEventListener('click', () => {
   menu.classList.toggle('show');
   menuIcon.classList.toggle('show-icon');
-  // logo.classList.toggle('visible');
-  closeBtn.classList.toggle('close');
-  // email.classList.toggle('visible');
+  logo.classList.toggle('visible');
+  closeBtn.classList.toggle('close');  
   nav.classList.toggle('change-width');
 });
