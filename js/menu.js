3940d90a4b9cd6608ec8d01efdf11cf3ba692f5a
Cesar Herrera <cesarherr321@gmail.com>
2023-04-20 01:17:20 -0400

    Update css and js files
    Add fixed isues whit linters

--- js/menu.js
+++ js/menu.js
@@ -1,19 +1,18 @@
 const menuIcon = document.querySelector('.hamburger-open');
 const menu = document.querySelector('.header__links--menu');
-const logo = document.querySelector('.header__links--logo');
 const closeBtn = document.querySelector('.hamburger-close');
 const nav = document.querySelector('.header__links');
 
 menuIcon.addEventListener('click', () => {
   menu.classList.toggle('show');
-  menuIcon.classList.toggle('show-icon'); 
-  closeBtn.classList.toggle('close'); 
+  menuIcon.classList.toggle('show-icon');
+  closeBtn.classList.toggle('close');
   nav.classList.toggle('change-width');
 });
 
 menu.addEventListener('click', () => {
   menu.classList.toggle('show');
-  menuIcon.classList.toggle('show-icon'); 
-  closeBtn.classList.toggle('close');  
+  menuIcon.classList.toggle('show-icon');
+  closeBtn.classList.toggle('close');
   nav.classList.toggle('change-width');
 });
